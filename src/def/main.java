671ab8c8d7d65ed90fb76adfcacee4272d683b38
psz <hwgzpsz@126.com>
2019-03-06 20:28:44 +0800

    psz 5 6

--- src/def/main.java
+++ src/def/main.java
@@ -8,6 +8,8 @@ public class main {
 		System.out.println("psz2");
 		System.out.println("psz3");
 		System.out.println("psz4");
+		System.out.println("psz5");
+		System.out.println("psz6");
 	}
 
 }
